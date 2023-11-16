4daa8aa8590e629a58de3e55f841ae44f15c34d4
mdelucasg <delucasgarcia.mario@gmail.com>
2023-11-16 04:02:23 +0100

    Update preprocessing

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,11 +1,10 @@
 import os
-import cv2
 import nltk
 import torch
 import numpy as np
 import pandas as pd
+from PIL import Image
 from torchvision import transforms
-from skimage.transform import resize
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 
@@ -17,6 +16,10 @@ PROJECTIONS_PATH = "data/indiana_projections.csv"
 REPORTS_PATH = "data/indiana_reports.csv"
 
 BATCH_SIZE = 16
+
+SOS_TOKEN = '<s>'
+EOS_TOKEN = '</s>'
+PAD_TOKEN = '<pad>'
 # ------------------Constants---------------------------------------------------
 projections = pd.read_csv(PROJECTIONS_PATH)
 projections = projections.set_index('uid')
@@ -24,53 +27,45 @@ projections = projections.set_index('uid')
 reports = pd.read_csv(REPORTS_PATH)
 reports = reports.set_index('uid')
 
+data = projections.copy().drop(['projection'], axis=1)
+for uid in reports.index:
+    report = reports.loc[uid]['findings']
+    data.loc[uid, 'report'] = report if type(report) == str else 'No findings.'
+
 # Global vocabulary
 vocab = set()
 
 class ChestXrayDataset(Dataset):
-    def __init__(self, projections, reports, uids, img_folder, pad_token='<pad>'):
-        self.projections = projections
-        self.reports = reports
+    def __init__(self, uids, img_folder, max_length=100, pad_token=PAD_TOKEN, transform=None):
+        self.uids = uids
+        self.transform = transform
+        self.data = data.loc[uids, :]
         self.img_folder = img_folder
-        self.data = self.__generate_data(uids)
         self.vocab, self.word2idx, self.idx2word = self.__create_vocab(pad_token)
 
     def __create_vocab(self, pad_token):
         vocab = set()
         vocab.add(pad_token)
-        for _, _, report in self.data:
+        for i in range(len(self.data)):
+            _, report  = self.data.iloc[i, :]
+            report = self.__sentence_tokenizer(report)
             vocab.update(report)
         word2idx = {word: idx for idx, word in enumerate(vocab)}
         idx2word = {idx: word for idx, word in enumerate(vocab)}
         return vocab, word2idx, idx2word
 
     def __sentence_tokenizer(self, text):
-        return ['<s>']+ nltk.word_tokenize(text) + ['</s>']
-
-    def __generate_data(self, uids):
-        data = []
-        for iter, uid in enumerate(uids):
-            print(f'Generating data: {iter+1}/{len(uids)}', end='\r')
-            try:
-                # Sometimes the report is not available
-                report = reports.loc[uid]['findings']
-                report = self.__sentence_tokenizer(report)
-                filename1, filename2 = self.projections.loc[uid]['filename']
-                img1 = load_image(os.path.join(self.img_folder, filename1))
-                img2 = load_image(os.path.join(self.img_folder, filename2))
-            except:
-                continue
-            data.append((img1, img2, report))
-        return data            
+        return [SOS_TOKEN]+ nltk.word_tokenize(text) + [EOS_TOKEN]          
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        img1, img2, report = self.data[idx]
-        report = [self.word2idx[word] for word in report]
-        # report = torch.LongTensor(report)
-        return img1, img2, report
+        filename, report = self.data.iloc[idx]
+        # report = self.__sentence_tokenizer(report)
+        img1 = load_image(os.path.join(self.img_folder, filename))
+        img1 = self.transform(img1)
+        return img1, report.lower()
 
     def get_vocab_size(self):
         return len(self.vocab)
@@ -78,15 +73,23 @@ class ChestXrayDataset(Dataset):
     def get_pad_token(self):
         return self.word2idx['<pad>']
 
-def create_dataloaders(uids, projections, reports, img_folder, batch_size=16):
+    def get_eos_token(self):
+        return self.word2idx['</s>']
+
+    def get_report(self, uid, to_idx=False):
+        report = reports.loc[uid]['findings']
+        report = report if type(report) == str else 'No findings'
+        return report
+
+def create_dataloaders(uids, img_folder, batch_size=16, transform=None):
     # Split UIDs into train, validation, and test sets
     train_uids, test_uids = train_test_split(uids, test_size=0.2, random_state=33)
     train_uids, val_uids = train_test_split(train_uids, test_size=0.25, random_state=33)  # 0.25 x 0.8 = 0.2
 
     # Create datasets
-    train_data = ChestXrayDataset(projections, reports, train_uids, img_folder)
-    val_data = ChestXrayDataset(projections, reports, val_uids, img_folder)
-    test_data = ChestXrayDataset(projections, reports, test_uids, img_folder)
+    train_data = ChestXrayDataset(train_uids, img_folder, transform=transform)
+    val_data = ChestXrayDataset(val_uids, img_folder, transform=transform)
+    test_data = ChestXrayDataset(test_uids, img_folder, transform=transform)
 
     # Create dataloaders
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=4)
@@ -95,7 +98,7 @@ def create_dataloaders(uids, projections, reports, img_folder, batch_size=16):
 
     return train_data, train_loader, val_data, val_loader, test_data, test_loader
 
-def get_sequences(data, eos_token, padding_token, padding_size=100):
+def padding_sequences(data, eos_token, padding_token, padding_size=100):
     sequences = []
     for _, _, report in data:
         if len(report) > padding_size:
@@ -119,10 +122,12 @@ def print_sequence(sequence, idx2word):
     print(' '.join([idx2word[idx] for idx in sequence]))
 
 def load_image(img_path):
-    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    imgr = resize(img, (2048, 2048))
-    imgr = np.expand_dims(imgr, axis=0)
-    return imgr
+    img = Image.open(img_path).convert('RGB')
+    # img = transform(img)
+    # img = img.unsqueeze(0)
+    # imgr = resize(img, (224, 224))
+    # img = np.expand_dims(img, axis=0)
+    return img
 
 def img2tensor(img):
     img_tensor = transforms.ToTensor()(img.astype(np.float32))
